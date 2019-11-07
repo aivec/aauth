4a0da78d84c8a86c9359bff26f2ed7598e4d3d5d
Evan Shaw <evandanielshaw@gmail.com>
2019-11-07 15:09:20 +0900

    fix: add deactivation hook for clearing cron
    BREAKING CHANGE: the class Scheduler now requires the plugin base file name as the last argument to its constructor

--- Scheduler.php
+++ Scheduler.php
@@ -19,8 +19,9 @@ class Scheduler extends Auth implements Scaffold {
      * @author Evan D Shaw <evandanielshaw@gmail.com>
      * @param string $sku
      * @param string $nag_display_name
+     * @param string $plugin_file  name of plugin entry file INCLUDING absolute path
      */
-    public function __construct($sku, $nag_display_name) {
+    public function __construct($sku, $nag_display_name, $plugin_file) {
         parent::__construct($sku);
 
         $this->nag_display_name = $nag_display_name;
@@ -38,6 +39,8 @@ class Scheduler extends Auth implements Scaffold {
                 wp_schedule_event($timestamp, 'daily', $sku . '_validate_install');
             }
         }
+
+        register_deactivation_hook($plugin_file, array($this, 'clearCron'));
     }
 
     /**
@@ -92,5 +95,15 @@ class Scheduler extends Auth implements Scaffold {
                 printf('<div class="%1$s"><p>%2$s</p></div>', esc_attr($class), $message);
             }
         }
+    }
+
+    /**
+     * Clear cron when plugin using Aauth is deactivated
+     *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @return void
+     */
+    public function clearCron() {
+        wp_clear_scheduled_hook($this->sku . '_validate_install');
     }
 }
